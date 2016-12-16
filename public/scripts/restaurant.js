299baa510835538cac823082aa24105c14c3b30d
tony2times2times <tony.peraza86@gmail.com>
2016-12-16 12:55:47 -0600

    pulling data off the DOM

--- public/scripts/restaurant.js
+++ public/scripts/restaurant.js
@@ -1,188 +1,102 @@
-// // arrays
-// var tables=[];
-// var employees=[];
-//
-// var createEmployee = function(){
-//   console.log( 'in createEmployee' );
-//   // get user input
-//   var employeeFirstName = document.getElementById( 'employeeFirstNameIn' ).value;
-//   var employeeLastName = document.getElementById( 'employeeLastNameIn' ).value;
-//   // create object for employee
-//   var newEmployee= {
-//     firstName : employeeFirstName,
-//     lastName : employeeLastName
-//   }; // end object
-//   // push into employees array
-//   employees.push( newEmployee );
-//   // update display
-//   listEmployees();
-// } // end createEmployee
-//
-// var createTable = function(){
-//   console.log( 'in createTable' );
-//   // get user input
-//   var tableName = document.getElementById('nameIn').value;
-//   var tableCapacity = document.getElementById('capacityIn').value;
-//   // table object for new table
-//   var newTable = {
-//     'name': tableName,
-//     'capacity': tableCapacity,
-//     'server': -1,
-//     'status': 'empty'
-//   }
-//   // push new obejct into tables array
-//   tables.push( newTable );
-//   console.log( 'added table: ' + newTable.name );
-//   // update output
-//   listTables();
-// } // end createTable
-//
-// var cycleStatus = function( index ){
-//   console.log( 'in cycleStatus: ' + index );
-//   // move table status to next status
-//   switch( tables[index].status ){
-//     case  'empty':
-//         tables[index].status = 'seated';
-//         break;
-//     case  'seated':
-//         tables[index].status = 'served';
-//         break;
-//     case  'served':
-//         tables[index].status = 'dirty';
-//         break;
-//     case  'dirty':
-//     default:
-//       tables[index].status = 'empty';
-//   }
-//   // show tables on DOM
-//   listTables();
-// } // end cycleStatus
-//
-// var listEmployees = function(){
-//   console.log( 'in listEmployees', employees );
-//   document.getElementById('employeesOutput').innerHTML = '<ul>';
-//   // loop through the tables array and display each table
-//   for( i=0; i< employees.length; i++ ){
-//     var line = employees[i].firstName + " " + employees[i].lastName + ', id: ' + i;
-//     // add line to output div
-//     document.getElementById('employeesOutput').innerHTML += '<li>' + line + '</li>';
-//   }
-//   document.getElementById('employeesOutput').innerHTML += '</ul>';
-//   // update tables display
-//   listTables();
-// } // end listEmployees
-//
-// var listTables = function(){
-//   console.log( "in listTables" );
-//   // target our output div
-//   document.getElementById('tablesOutput').innerHTML = '';
-//   // loop through the tables array and display each table
-//
-//   // select to assign a server to this table
-//   var selectText = '<select>';
-//   for (var i = 0; i < employees.length; i++) {
-//     selectText+= '<option value=' + i + '>'+ employees[i].firstName + ' ' + employees[i].lastName + '</option>';
-//   }
-//   selectText += '</select>';
-//   // display employees
-//   for( i=0; i< tables.length; i++ ){
-//     // status is a button that, when clicked runs cycleStatus for this table
-//     var line = tables[i].name + " - capacity: " + tables[i].capacity + ', server: ' + selectText + ', status: <button onClick="cycleStatus(' + i + ')">' + tables[i].status + "</button>";
-//     // add line to output div
-//     document.getElementById('tablesOutput').innerHTML += '<p>' + line + '</p>';
-//   }
-// } // end listTables
+//Global arrays
+var mesa=[];
+var employees=[];
 
-var getWaitstaff = function (){
-  $.ajax({
-    type: "GET",
-    url: '/getWaitstaff',
-    success: function ( response ){
-      console.log('back from get call:', response);
-    },//end success
-    error: function (){
-      console.log("error with ajax call");
-    } //end error
-  });//end ajax call
-};//end get data
+function getWaitstaff() {
+  mesa=[];
+    $.ajax({
+        type: "GET",
+        url: '/getWaitstaff',
+        success: function(response) {
+            console.log('back from get call:', response);
+            for (var i = 0; i < response.length; i++) {
+              mesa.push(response[i]);
+              console.log('mesa array: ' + mesa);
+            }
+        }, //end success
+        error: function() {
+                console.log("error with ajax call");
+            } //end error
+    }); //end ajax call
+} //end get data
 
-var getMesa = function (){
-  $.ajax({
-    type: "GET",
-    url: '/getMesa',
-    success: function ( response ){
-      console.log('back from get call:', response);
-    },//end success
-    error: function (){
-      console.log("error with ajax call");
-    } //end error
-  });//end ajax call
-};//end get data
+function getMesa() {
+    $.ajax({
+        type: "GET",
+        url: '/getMesa',
+        success: function(response) {
+            console.log('back from get call:', response);
+        }, //end success
+        error: function() {
+                console.log("error with ajax call");
+            } //end error
+    }); //end ajax call
+} //end get data
 
-getMesa();
+function postWaitstaff() {
+  var newWaitstaff ={
+      first_name: $('#employeeFirstNameIn').val,
+      last_name: $('#employeeLastNameIn').val,
+      active: true
+    };
+
+    $.ajax({
+        type: "POST",
+        url: '/postWaitstaff',
+        data: testObject,
+        success: function(response) {
+            console.log('back from post call:', response);
+        }, //end success
+        error: function() {
+                console.log("error with ajax call");
+            } //end error
+    }); //end ajax call
+} //end post data
+
+var postMesa = function() {
+  var newTable ={
+    mesa_number: $('#mesaNumberIn').val,
+    capacity:  $('#capacityIn').val
+  };
+    $.ajax({
+        type: "POST",
+        url: '/postMesa',
+        data: newTable,
+        success: function(response) {
+            console.log('back from post call:', response);
+        }, //end success
+        error: function() {
+                console.log("error with ajax call");
+            } //end error
+    }); //end ajax call
+}; //end post data
+
+var putMesa = function() {
+    var updateMesa = {
+        mesa_number: 42,
+        waitstaff_id: 3,
+        mesa_status: 'served'
+    };
+    $.ajax({
+        type: "PUT",
+        url: '/putMesa',
+        data: updateMesa,
+        success: function(response) {
+            console.log('back from put call:', response);
+        }, //end success
+        error: function() {
+                console.log("error with ajax call");
+            } //end error
+    }); //end ajax call
+}; //end post data
+
+function enable(){
+
+}
 
 
-
-var postWaitstaff = function (){
-  var testObject = {
-    first_name: 'mickey',
-    last_name: 'mouse',
-    active: true
-  }; //for test purposes only
-  $.ajax({
-    type: "POST",
-    url: '/postWaitstaff',
-    data: testObject,
-    success: function ( response ){
-      console.log('back from post call:', response);
-    },//end success
-    error: function (){
-      console.log("error with ajax call");
-    } //end error
-  });//end ajax call
-};//end post data
-
-var postMesa = function (){
-
-     //for test purposes only
-  $.ajax({
-    type: "POST",
-    url: '/postMesa',
-    data: testObject,
-    success: function ( response ){
-      console.log('back from post call:', response);
-    },//end success
-    error: function (){
-      console.log("error with ajax call");
-    } //end error
-  });//end ajax call
-};//end post data
-
-var putMesa = function (){
-
-var testObject= {
-  mesa_number: 42,
-  waitstaff_id: 3,
-  mesa_status: 'served'
-};
-  $.ajax({
-    type: "PUT",
-    url: '/putMesa',
-    data: testObject,
-    success: function ( response ){
-      console.log('back from put call:', response);
-    },//end success
-    error: function (){
-      console.log("error with ajax call");
-    } //end error
-  });//end ajax call
-};//end post data
-
-
-
-
-$(function () {
-   console.log('document is ready');
-   putMesa();
- });
+$(function() {
+    console.log('document is ready');
+    getWaitstaff();
+});
 console.log('js loaded');
